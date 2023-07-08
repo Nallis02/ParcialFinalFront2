569f36ddf0767c50c3c492a9e2a182b87c23ba7f
Nallive Trujillo <nallive14@gmai.com>
2023-07-07 20:19:54 -0500

    recuperatorio test de componente cita

--- src/mocks/quotes.mocks.ts
+++ src/mocks/quotes.mocks.ts
@@ -1,19 +1,29 @@
-export const quote = [
+export const mockedCitas =[
   {
-    quote: "By chilling my loins I increase the chances of impregnating my wife.",
-    character: "Apu Nahasapeemapetilon",
-    image:
-      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FApuNahasapeemapetilon.png?1497567513002",
-    characterDirection: "Right",
+      query:"Moe",
+      data:{
+          quote: "Well, I'm better than dirt. Well, most kinds of dirt. I mean not that fancy store bought dirt. That stuffs loaded with nutrients. I.. I can't compete with that stuff.",
+          character: "Moe Szyslak",
+          image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMoeSzyslak.png?1497567512411",
+          characterDirection: "Right"
+      },
   },
-];
-export const quoteLisa = [
   {
-    quote:
-      "These are my only friends...grown-up nerds like Gore Vidal. And even he's kissed more boys than I ever will",
-    character: "Lisa Simpson",
-    image:
-      "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512411",
-    characterDirection: "Right",
-  },
-];
+    query:"Homer",
+    data:{
+        quote: "Oh, so they have Internet on computers now!",
+        character: "Homer Simpson",
+        image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+        characterDirection: "Right"
+    }
+},
+{
+    query:"Apu",
+    data:{
+        quote: "By chilling my loins I increase the chances of impregnating my wife.",
+        character: "Apu Nahasapeemapetilon",
+        image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2ApuNahasapeemapetilon.png?1497567511523",
+        characterDirection: "Left"
+    }
+}
+]
--- src/tests/Cita.test.tsx
+++ src/tests/Cita.test.tsx
@@ -1,70 +1,137 @@
 import { render } from "../test-utils";
-import fetchMock from "fetch-mock";
 import Cita from "../features/quote/Cita";
-import { fireEvent, screen, waitFor } from "@testing-library/react";
-import { test } from "@jest/globals";
+import { screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import { quoteLisa } from "../mocks/quotes.mocks";
+import { setupServer } from "msw/lib/node";
+import { mockedCitas } from "../mocks/quotes.mocks";
+import { API_URL } from "../app/constants";
+import { rest } from "msw";
+import userEvent from '@testing-library/user-event';
+import {MENSAJE_CARGANDO, NOMBRE_INVALIDO, NO_ENCONTRADO} from '../features/quote/constants'
 
-describe("Prueba en Componente Cita", () => {
-  beforeAll(() => {
-    fetchMock.mock(
-      "https://thesimpsonsquoteapi.glitch.me/quotes?character=lisa",
-      quoteLisa
+
+const randomQuote = mockedCitas[0].data;
+const validQueries = mockedCitas.map((q)=> q.query);
+
+const handlers = [
+  rest.get(`${API_URL}`, (req, res, ctx) => {
+    const character = req.url.searchParams.get('character');
+
+    if (character === null) {
+      return res(ctx.json([randomQuote]), ctx.delay(150));
+    }
+
+    if (validQueries.includes(character)) {
+      const quote = mockedCitas.find((quote) => quote.query === character);
+      return res(ctx.json([quote?.data]));
+    }
+
+    return res(ctx.json([]), ctx.delay(150));
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
+const renderPorDefecto=()=>{
+    render(
+        <Cita/>
     );
-  });
-
-  afterAll(() => {
-    fetchMock.restore();
-  });
-
-  test("Al ingresar un nombre debe aparecer CARGANDO...", () => {
-    render(<Cita />);
-    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    fireEvent.change(input, { target: { value: "lisa" } });
-    const button = screen.getByRole("button", { name: "Obtener Cita" });
-    fireEvent.click(button);
-    expect(screen.getByText("CARGANDO...")).toBeInTheDocument();
-  });
-
-  test("El boton borra la cita del autor que se busca", async () => {
-    render(<Cita />);
-    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    fireEvent.change(input, { target: { value: "lisa" } });
-    const button = screen.getByRole("button", { name: "Obtener Cita" });
-    fireEvent.click(button);
-
-    await waitFor(() => {
-      expect(screen.queryByText("CARGANDO...")).not.toBeInTheDocument();
+}
+describe('Citas', () => {
+    describe('Se renderiza la pagina', () => {
+        it('No debe mostrar ninguna cita', async () => {
+            renderPorDefecto();
+            expect(screen.queryByText(/Moe Szyslak/i)).not.toBeInTheDocument();
+        });
+        it('Debe mostrar el boton de borrar',()=>{
+            renderPorDefecto();
+            expect(screen.getByRole('button',{name: /Borrar/i})).toBeInTheDocument();
+        });
+        it('Debe mostrar el boton de "Obtener cita aleatoria"',()=>{
+            renderPorDefecto();
+            expect(screen.getByRole('button',{name: /Obtener cita aleatoria/i })).toBeInTheDocument();
+        });
+        it('Cuando se ingresa algo en el input el boton de "obtener cita ateatoria" debe cambiar a "Obtener Cita"', async()=>{
+            renderPorDefecto();
+            const input = screen.getByRole('textbox', {name:'Author Cita'});
+            userEvent.click(input);
+            await userEvent.type(input,"Moe")
+            const botonObtenerCita = await screen.findByRole('button', {name:/Obtener Cita/i});
+            expect(botonObtenerCita).toBeInTheDocument();
+        });
+    });
+    describe('Cuando se carga una cita correcta o que existe',() => {
+        
+        it('Carga la palabra CARGANDO... antes de mostrar la cita aletoria', async() =>{
+            renderPorDefecto();
+            const botonObtenerCita = screen.getByRole('button',{name: /Obtener cita aleatoria/i } );
+            userEvent.click(botonObtenerCita);
+            await waitFor(()=>{
+                expect(screen.getByText(MENSAJE_CARGANDO)).toBeInTheDocument()
+            })
+        })
+        it('Muestra una cita de un personaje aleatorio', async() =>{
+            renderPorDefecto();
+            const botonObtenerCita = screen.getByRole('button',{name: /Obtener cita aleatoria/i } );
+            userEvent.click(botonObtenerCita);
+            await waitFor(()=>{
+                expect(screen.getByText(/Moe Szyslak/i)).toBeInTheDocument()
+            })
+        })
+        it('Cita de personaje al ingresar un nombre valido', async() =>{
+            renderPorDefecto();
+            const input = screen.getByRole('textbox', {name:'Author Cita'});
+            userEvent.click(input);
+            await userEvent.type(input,"Homer")
+            const botonObtenerCita = await screen.findByText(/Obtener Cita/i);
+            userEvent.click(botonObtenerCita);
+            await waitFor(()=>{
+                expect(screen.getByText(mockedCitas[1].data.quote)).toBeInTheDocument()
+            })
+        })
+        
     });
 
-    await waitFor(() => {
-      expect(screen.getByText("Lisa Simpson")).toBeInTheDocument();
-    });
-
-    const cleanButton = screen.getByRole("button", { name: "Borrar" });
-    fireEvent.click(cleanButton);
-    expect(screen.getByText("No se encontro ninguna cita")).toBeInTheDocument();
-  });
-
-  test("Busqueda de cita por personaje", () => {
-    render(<Cita />);
-    expect(screen.getByText("Obtener cita aleatoria")).toBeInTheDocument();
-  });
-
-  test("Busqueda de una cita por personaje", async () => {
-    render(<Cita />);
-    const input = screen.getByPlaceholderText("Ingresa el nombre del autor");
-    fireEvent.change(input, { target: { value: "lisa" } });
-    const button = screen.getByRole("button", { name: "Obtener Cita" });
-    fireEvent.click(button);
-
-    await waitFor(() => {
-      expect(screen.queryByText("CARGANDO...")).not.toBeInTheDocument();
-    });
-
-    await waitFor(() => {
-      expect(screen.getByText("Lisa Simpson")).toBeInTheDocument();
-    });
-  });
+    describe("Se ingresan datos incorrectos", ()=>{
+        it('Se ingresa numeros en el input', async() =>{
+            renderPorDefecto();
+            const input = screen.getByPlaceholderText('Ingresa el nombre del autor');
+            userEvent.click(input);
+            await userEvent.type(input,"1")
+            const botonObtenerCita = screen.getByRole('button', {name: /Obtener Cita/i});
+            userEvent.click(botonObtenerCita);
+            await waitFor(()=>{
+                expect(screen.getByText(/El nombre debe ser un texto/i)).toBeInTheDocument()
+            })
+        })
+        it('Se ingresa un personaje que no existe, en el input', async() =>{
+            renderPorDefecto();
+            const input = screen.getByRole('textbox', {name:'Author Cita'});
+            userEvent.click(input);
+            await userEvent.type(input,"lissa")
+            const botonObtenerCita = await screen.findByText(/Obtener Cita/i);
+            userEvent.click(botonObtenerCita);
+            await waitFor(()=>{
+                expect(screen.getByText(NOMBRE_INVALIDO)).toBeInTheDocument()
+            })
+        })
+    })
+    describe("Boton de borrar", ()=>{
+        it("El boton borra la cita del autor que se busca y limpia el input, vuelve al estado inicial", async()=>{
+            renderPorDefecto();
+            const botonObtenerCita = await screen.findByText(/Obtener cita aleatoria/i);
+            userEvent.click(botonObtenerCita);
+            const botonBorrar = await screen.findByLabelText(/Borrar/i)
+            userEvent.click(botonBorrar);
+            await waitFor(()=>{
+                expect(screen.getByText(NO_ENCONTRADO)).toBeInTheDocument()
+            })
+        })
+    })
 });
